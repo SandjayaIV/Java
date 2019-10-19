95323d3d18db3e8dad9e5c526b9a73cad884b3cd
SandjayaIV <stanislasvallet@live.com>
2019-10-19 15:24:38 +0200

    exceptions personnalisées

--- coursJava/Gestion des erreurs/src/gerreur/Propagation.java
+++ coursJava/Gestion des erreurs/src/gerreur/Propagation.java
@@ -5,4 +5,5 @@ public class Propagation {
         int r = a/b;
         return r;
     }
+
 }
--- coursJava/Gestion des erreurs/src/main.java
+++ coursJava/Gestion des erreurs/src/main.java
@@ -1,11 +1,15 @@
 import gerreur.Propagation;
+import gerreur.Quotient;
 
 public class main {
     public static void main(String[] args) {
         int a = 8;
         int b = 0;
-        Propagation p = new Propagation();
-        System.out.println(p.divide(a,b));
+        Quotient q = new Quotient();
+
+        int result = q.fact(-3);
+        System.out.println(result);
+
 
     }
 }
