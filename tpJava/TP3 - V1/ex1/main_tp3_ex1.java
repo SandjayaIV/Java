d067d4c1e558626fbe64084e567617670a13a6c9
stanislasvallet <stanislasvallet@live.com>
2019-10-19 13:37:33 +0200

    suppression tp03 et tp03 V1

--- tpJava/TP3 - V1/ex1/main_tp3_ex1.java
+++
@@ -1,7 +0,0 @@
-public class main_tp3_ex1 {
-    public static void main(String[] args) {
-        Ville v = new Ville("Les Pennes-Mirabeau",19022);
-        v.obtenirInformation();
-
-    }
-}
--- tpJava/TP3 - V1/ex3/main_tp3_ex3.java
+++
@@ -1,5 +0,0 @@
-public class main_tp3_ex3{
-    public static void main(String[] args) {
-        
-    }
-}
